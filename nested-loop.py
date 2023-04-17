8ed10fdb9782ef1f091f91d000ee104f0033a059
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-04-16 22:58:57 -0400

    Create nested-loop.py

---
+++ nested-loop.py
@@ -0,0 +1,28 @@
+# This code is written in the Python programming language, and it counts the number of "other" items in a group of 2 items.
+
+# Let's break it down line by line:
+
+
+others = 0
+
+#This line sets the variable others equal to 0. This variable will be used to keep track of the number of "other" items.
+
+for i in range(2):
+    for j in range(2):
+        if i != j:
+            others += 1
+# These three lines use two nested loops to compare every pair of items in the group. 
+# The range(2) function generates the numbers 0 and 1, so i and j will take on the values 0 and 1 in turn. The if i != j statement checks if the two items are different, and if they are, it increments the others variable by 1. 
+# This loop will execute a total of 4 times, checking every possible pair of items.
+
+else:
+    others += 1
+    
+# This else statement is not related to the if statement in the previous loop. 
+# It is actually attached to the for loop itself, and is executed when the loop finishes normally (i.e. without hitting a break statement). 
+# In this case, it increments the others variable by 1.
+
+print(others)
+
+# Finally, this line prints the value of the others variable, which should be equal to 3. 
+# This is because there are 3 "other" items in a group of 2: item 1 can be paired with item 2 or item 3, and item 2 can be paired with item 1 or item 3.
