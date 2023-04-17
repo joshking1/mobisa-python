9b126614676f5795b938ebcaa5523ff7af40ee47
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-04-16 23:01:05 -0400

    Create nested-loop-2.py

---
+++ nested-loop-2.py
@@ -0,0 +1,30 @@
+# Let's break it down line by line:
+
+
+others = 0
+
+# This line sets the variable others equal to 0. This variable will be used to keep track of the number of times i is equal to j.
+
+for i in range(1, 3):
+    for j in range(1, 2):
+        if i == j:
+            others += 1
+            
+# These three lines use two nested loops to compare every pair of values i and j. The range(1, 3) function generates the numbers 1 and 2, 
+# so i will take on the values 1 and 2 in turn. The range(1, 2) function generates only the number 1, so j will take on the value 1 in both iterations of the loop. 
+# The if i == j statement checks if the two values are equal, and if they are, it increments the others variable by 1. 
+# This loop will execute a total of 2 times, checking the pairs (1,1) and (2,1).
+
+else:
+    others += 1
+    
+# This else statement is not related to the if statement in the previous loop. 
+# It is actually attached to the for loop itself, and is executed when the loop finishes normally (i.e. without hitting a break statement). 
+# In this case, it increments the others variable by 1 after each iteration of the inner loop, so it will execute a total of 2 times and add 2 to the others variable.
+
+
+print(others)
+
+# Finally, this line prints the value of the others variable, which should be equal to 4. 
+# This is because i is equal to j once (the pair (1,1)), and the else statement adds 2 to the others variable after each iteration of the inner loop, 
+# so it adds 2 for each of the 2 iterations of the outer loop. Therefore, the total number of times i is equal to j is 1 + 2 + 2 = 4.
